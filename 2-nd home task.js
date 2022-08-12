d8dfdd783e5213acd4c61836f8b52e926fb6ad17
Artem-Dubovyk <dubovik.artyom@gmail.com>
2022-08-12 12:41:36 +0300

    2 part complete

--- 2-nd home task.js
+++ 2-nd home task.js
@@ -1,4 +1,5 @@
 //превратить строку "Hello world" в "Hello World!"
+
 const welcomeText = "Hello world";
 //create temporary arr with words
 let temp = welcomeText.split(' ');
@@ -18,3 +19,24 @@ console.log(temp);
 //prepare new sentence
 const capitalizeWelcomeText = temp[0]+' '+temp[1]+'!';
 console.log(capitalizeWelcomeText);
+
+
+//написать цикл который будет логировать тип данных каждого символа строки (например "a123" - в итоге должна быть строка "string, number, number. number"(Внимание именно в таком формате - иными словами в один ряд))
+
+const strChecked = "a123";
+let arrWords = strChecked.split("");
+console.log(arrWords);
+
+var typeStrData = [];
+
+for(let i2 = 0; i2 < arrWords.length; i2++){
+    var re = /[0-9]/;
+    if(re.test(arrWords[i2])){
+        typeStrData.push("Number");
+    }else{
+        typeStrData.push("String");
+    }
+};
+console.log(typeStrData);
+const strInfoType = typeStrData.join(", ");
+console.log(strInfoType);
