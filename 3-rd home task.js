96e973e4a44f4ab284dff094af67619ec8e8c68e
Artem-Dubovyk <dubovik.artyom@gmail.com>
2022-08-17 20:26:53 +0300

    update 3-d homework

--- 3-rd home task.js
+++ 3-rd home task.js
@@ -57,6 +57,7 @@ const usersArray = [
 //prepare balance in arr
 let arrTemp = JSON.stringify(usersArray);
 arrTemp = JSON.parse(arrTemp);
+
 //convert balance in number
 arrTemp.forEach(x => {
     x.balance = parseInt(x.balance);
@@ -64,10 +65,12 @@ arrTemp.forEach(x => {
         x.friends.forEach(z => z.balance = parseInt(z.balance));
     }
 });
+
 //select top balance user's
 let topBalance = arrTemp.reduce((prev,item) => {
     return item.balance > prev.balance ? item : prev;
 });
+
 //top balance user
 console.log(topBalance.name);
 
@@ -80,15 +83,93 @@ let arrSortNames = usersArray.sort((a, b) => {
         return 1;
     }
 });
+
 //show sort arr for names
 console.log(arrSortNames);
 
 //3) Вывести общий баланс пользователей
 
 let totalAmount = 0;
+
 //use arr with convert balances
 arrTemp.forEach(x => totalAmount += x.balance);
+
 //show total balance users
 console.log(totalAmount);
 
 //4) Удалить пользователя Russell
+
+//check index elem with name = "Russell"
+let indexDeletedElement = usersArray.findIndex((element) => element.name === "Russell");
+
+//delete element with name "Russell"
+usersArray.splice(indexDeletedElement, 1);
+console.log(usersArray);
+
+//5) Удалить пользователя Isaac
+indexDeletedElement = null;
+//find index elem with name "Isaac"
+indexDeletedElement = usersArray.findIndex((element) => element.name === "Isaac");
+
+//delete elem
+usersArray.splice(indexDeletedElement, 1);
+console.log(usersArray);
+
+//6) Добавить пользователя Harry после John
+let indexAddElem = usersArray.findIndex((element) => element.name === "John");
+usersArray.splice(indexAddElem +1, 0, {"name": "Harry", "balance": '0$', "friends": []});
+//arr with add elem Harry after John
+console.log(usersArray);
+
+//7) Добавить пользователя Conor в конец массива
+usersArray.push({"name": "Conor", "balance": '5000$', "friends": []});
+
+console.log(usersArray);
+
+//8) Вывести массив всех пользователей включая друзей без повторения
+const arrFriends = usersArray.map(x => x.friends);
+arrFriends.flat();
+const arrAllUsers = usersArray.concat(arrFriends.flat());
+console.log(arrAllUsers);
+
+//9) Вывести массив пользователей у которых баланс больше 2000$
+//prepare updated arr with new users
+arrTemp = null;
+arrTemp = JSON.stringify(usersArray);
+arrTemp = JSON.parse(arrTemp);
+
+//convert balance in number
+arrTemp.forEach(x => {
+    x.balance = parseInt(x.balance);
+    if(x.friends){
+        x.friends.forEach(z => z.balance = parseInt(z.balance));
+    }
+});
+
+//select rich users
+let richUsers = [];
+richUsers = arrTemp.filter(x => x.balance > 2000);
+console.log(richUsers);
+
+//10) Вывести имя самого богатого пользователя(включая друзей)
+
+//convert balance in number
+//set up value arr with all ysers (with friends)
+arrTemp = null;
+arrTemp = JSON.stringify(arrAllUsers);
+arrTemp = JSON.parse(arrTemp);
+
+//convert balance in number
+arrTemp.forEach(x => {
+    x.balance = parseInt(x.balance);
+    if(x.friends){
+        x.friends.forEach(z => z.balance = parseInt(z.balance));
+    }
+});
+
+const mostRichUser = arrTemp.reduce((prev, item) => {
+    return item.balance > prev.balance ? item : prev
+});
+//show name most rich user
+console.log(mostRichUser.name)
+
