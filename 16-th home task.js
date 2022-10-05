b16c1c9e64d9ceee09108e3ee414e96f89b39e9e
adubovyk <artem.dubovyk@fintech-farm.com>
2022-10-05 20:19:12 +0300

    fix 16-th task

--- 16-th home task.js
+++ 16-th home task.js
@@ -1,18 +1,11 @@
 /*1) Реализовать функцию-генератор которая отдаст 10 рандомных чисел в диапазоне переданном в аргументах в ней.*/
 
 function* generateRandomNum(min, max) {
-    console.log(min, max)
-    yield createRandomNum(min, max);
-    yield createRandomNum(min, max);
-    yield createRandomNum(min, max);
-    yield createRandomNum(min, max);
-    yield createRandomNum(min, max);
-    yield createRandomNum(min, max);
-    yield createRandomNum(min, max);
-    yield createRandomNum(min, max);
-    yield createRandomNum(min, max);
-    yield createRandomNum(min, max);
 
+    const from = 0;
+    const to = 9;
+
+    for (let i = from; i <= to; i++) yield createRandomNum(min, max);
 
     function createRandomNum(min, max) {
         min = Math.ceil(min);
@@ -21,30 +14,29 @@ function* generateRandomNum(min, max) {
     }
 };
 
-let myGenerate = generateRandomNum(1,9);
+let myGenerate = generateRandomNum(1, 9);
 
-for(let value of myGenerate){
+for (let value of myGenerate) {
     console.log(value);
 }
 
 
 /*2) Создать свой распорядок дня с помощью async await ( тоесть мы должны "подождать" выполнение асинхронной функции brushTeeth 
 перед тем как приступать к выполнению следующей асинхронной функции)*/
-function* generateSchedule(){
-    yield "09:00 brush teeth";
-    yield "09:30 breakfest";
-    yield "10:00 start work";
+
+async function getSchedule() {
+    let firstItem = await new Promise(function (resolve) {
+        resolve("09:00 brush teeth");
+    });
+
+    let secondItem = await new Promise(function (resolve) {
+        resolve("09:30 breakfest");
+    });;
+    let thirdItem = await new Promise(function (resolve) {
+        resolve("10:00 start work");
+    });
+
+    return `My morning schedule: 1)${firstItem}; 2)${secondItem}; 3)${thirdItem}`;
 };
 
-let generateMySchedule = generateSchedule();
-
-async function getSchedule(){
-    let firstItem = await generateMySchedule.next();
-    let secondItem = await generateMySchedule.next();
-    let thirdItem = await generateMySchedule.next();
-    return `My morning schedule: 1)${firstItem.value}; 2)${secondItem.value}; 3)${thirdItem.value}`;
-}
-
-
-let mySchedule = await getSchedule();
-console.log(mySchedule)
+console.log(await getSchedule());
