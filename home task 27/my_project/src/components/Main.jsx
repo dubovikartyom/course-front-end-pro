de98b01f989bb71bd2ac121f2a025ac3c1bb6c25
adubovyk <artem.dubovyk@fintech-farm.com>
2022-11-29 22:18:29 +0200

    fix task #28

--- home task 27/my_project/src/components/Main.jsx
+++ home task 27/my_project/src/components/Main.jsx
@@ -1,28 +1,46 @@
 import "../css/Main.css";
 import React, { Component } from "react";
-import getList from "../helpers/starWarsAPI.js";
+import axios from "axios";
+import Card from "./common/Card"
 
-const productList = JSON.stringify(getList());
 
 export class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      getListproduct: false
+      productList: null
     };
   }
 
+  getList() {
+    axios.get("https://swapi.dev/api/starships/").then((res) => {
+      const o_productList = res.data.results;
+      this.setState({productList: o_productList});
+    });
+  }
+  
+  componentDidMount() {
+    this.getList()
+  }
+  
+
 
   render() {
-    if (this.state.getListproduct) {
+
+    if (this.state.productList != null) {
       return(
-        <div className="main">
-          <div className="card">{JSON.parse(productList).results[0].name}</div>
+        <div className="main with_products">
+          <Card item={this.state.productList[0]}/>
+          <Card item={this.state.productList[1]}/>
+          <Card item={this.state.productList[2]}/>
+          <Card item={this.state.productList[3]}/>
+          <Card item={this.state.productList[4]}/>
+          <Card item={this.state.productList[5]}/>
         </div>
       )
     } else {
       return (
-        <div className="main" onLoad={setInterval(() => { this.setState({getListproduct:true}) }, 3000)}>
+        <div className="main" onLoad={this.componentDidMount()}>
           <div class="container">
             <div class="block"></div>
             <div class="block"></div>
---
+++ home task 27/my_project/src/components/common/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../../css/common/card.css";
+
+function Card(props) {
+
+  return (
+    <div className="card">
+        <div className="card_pic"><img src="https://www.denofgeek.com/wp-content/uploads/2016/01/millennium-falcon.jpg" alt="ship" /></div>
+        <div className="card_name place">Name: {props.item.name}</div>
+        <div className="card_model place">Model: {props.item.model}</div>
+        <div className="card_manufacturer place">Manufacturer: {props.item.manufacturer}</div>
+        <div className="card_speed place">Speed: {props.item.max_atmosphering_speed}</div>
+        <div className="card_cost place">Cost: {props.item.cost_in_credits}</div>
+    </div>
+  )
+}
+
+export default Card
